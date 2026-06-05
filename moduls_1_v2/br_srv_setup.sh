ccb2761f83d3c81425d756ea24d42adb69315653
BR67 <152090761+BR67test@users.noreply.github.com>
2026-06-06 00:12:41 +0500

    Update br_srv_setup.sh

--- moduls_1_v2/br_srv_setup.sh
+++ moduls_1_v2/br_srv_setup.sh
@@ -12,14 +12,14 @@ NM_CONTROLLED=no
 SYSTEMD_CONTROLLED=yes
 DISABLED=no
 EOF
-echo "192.168.200.2/27" > /etc/net/ifaces/enp7s1/ipv4address
-echo "default via 192.168.200.1" > /etc/net/ifaces/enp7s1/ipv4route
-echo "nameserver 8.8.8.8" > /etc/net/ifaces/enp7s1/resolv.conf
+echo "192.168.0.2/28" > /etc/net/ifaces/enp7s1/ipv4address
+echo "default via 192.168.0.1" > /etc/net/ifaces/enp7s1/ipv4route
+echo "nameserver 77.88.8.8" > /etc/net/ifaces/enp7s1/resolv.conf
 
 systemctl restart network
 
-# Пользователь
-useradd sshuser -u 1010
+# Пользователь sshuser
+useradd sshuser -u 2026
 echo "sshuser:P@ssw0rd" | chpasswd
 usermod -aG wheel sshuser
 echo "sshuser ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
@@ -27,7 +27,7 @@ echo "sshuser ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 # SSH
 apt-get update && apt-get install -y openssh-server
 cat > /etc/openssh/sshd_config <<SSH
-Port 2024
+Port 2026
 MaxAuthTries 2
 PermitRootLogin no
 AllowUsers sshuser
@@ -38,6 +38,7 @@ echo "Authorized access only" > /etc/openssh/banner
 systemctl restart sshd
 
 # Время
+apt-get install -y tzdata
 timedatectl set-timezone Asia/Yekaterinburg
 
 echo "=== BR-SRV готов ==="
