aba834a0732a8a64348b4554fafcc8ebd80b0610
BR67 <152090761+BR67test@users.noreply.github.com>
2026-06-06 00:11:50 +0500

    Update br_rtr_setup.sh

--- moduls_1_v2/br_rtr_setup.sh
+++ moduls_1_v2/br_rtr_setup.sh
@@ -12,8 +12,9 @@ NM_CONTROLLED=no
 SYSTEMD_CONTROLLED=yes
 DISABLED=no
 EOF
-echo "172.16.5.14/28" > /etc/net/ifaces/enp7s1/ipv4address
-echo "default via 172.16.5.1" > /etc/net/ifaces/enp7s1/ipv4route
+echo "172.16.2.2/28" > /etc/net/ifaces/enp7s1/ipv4address
+echo "default via 172.16.2.1" > /etc/net/ifaces/enp7s1/ipv4route
+echo "nameserver 77.88.8.8" > /etc/net/ifaces/enp7s1/resolv.conf
 
 # enp7s2 — к BR-SRV
 mkdir -p /etc/net/ifaces/enp7s2
@@ -24,22 +25,25 @@ NM_CONTROLLED=no
 SYSTEMD_CONTROLLED=yes
 DISABLED=no
 EOF
-echo "192.168.200.1/27" > /etc/net/ifaces/enp7s2/ipv4address
+echo "192.168.0.1/28" > /etc/net/ifaces/enp7s2/ipv4address
 
 systemctl restart network
 
 # GRE-туннель
-ip tunnel add gre1 mode gre local 172.16.5.14 remote 172.16.4.14 ttl 255
+ip tunnel add gre1 mode gre local 172.16.2.2 remote 172.16.1.2 ttl 64
 ip link set gre1 up
 ip addr add 10.10.10.2/30 dev gre1
 
+# Форвардинг
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
+
 # NAT
-iptables -t nat -A POSTROUTING -s 192.168.200.0/27 -o enp7s1 -j MASQUERADE
+apt-get update && apt-get install -y iptables
+iptables -t nat -A POSTROUTING -o enp7s1 -j MASQUERADE
 iptables-save > /etc/sysconfig/iptables
 systemctl enable --now iptables
 
-# Форвардинг
-echo 1 > /proc/sys/net/ipv4/ip_forward
 
 # FRR + OSPF
 apt-get update && apt-get install -y frr
@@ -49,22 +53,25 @@ systemctl restart frr
 vtysh <<VTYSH
 conf t
 router ospf
- ospf router-id 10.10.10.2
+ ospf router-id 192.168.0.1
+ passive-interface default
  network 10.10.10.0/30 area 0
- network 172.16.5.0/28 area 0
- network 192.168.200.0/27 area 0
+ network 192.168.0.0/28 area 0
 interface gre1
+ no ip ospf passive
  ip ospf authentication message-digest
  ip ospf message-digest-key 1 md5 P@ssw0rd
 do wr mem
 VTYSH
 
-# Пользователь
+# Пользователь net_admin
 useradd net_admin -u 1010
-echo "net_admin:P@\$\$word" | chpasswd
+echo "net_admin:P@ssw0rd" | chpasswd
 echo "net_admin ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # Время
+apt-get install -y tzdata
 timedatectl set-timezone Asia/Yekaterinburg
+
 
 echo "=== BR-RTR готов ==="
