a44c5b98c884777d7b0da97ca681109a26a74685
BR67 <152090761+BR67test@users.noreply.github.com>
2026-06-06 00:07:02 +0500

    Update isp_setup.sh

--- moduls_1_v2/isp_setup.sh
+++ moduls_1_v2/isp_setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 echo "=== ISP ==="
 
-hostnamectl set-hostname isp.au-team.irpo
+hostnamectl set-hostname isp
 
-# Сеть
+# Настройка интерфейсов
 mkdir -p /etc/net/ifaces/enp7s1
-echo "BOOTPROTO=dhcp" > /etc/net/ifaces/enp7s1/options
+cat > /etc/net/ifaces/enp7s1/options <<EOF
+BOOTPROTO=dhcp
+TYPE=eth
+NM_CONTROLLED=no
+SYSTEMD_CONTROLLED=yes
+DISABLED=no
+EOF
 
 mkdir -p /etc/net/ifaces/enp7s2
 cat > /etc/net/ifaces/enp7s2/options <<EOF
@@ -15,7 +21,7 @@ NM_CONTROLLED=no
 SYSTEMD_CONTROLLED=yes
 DISABLED=no
 EOF
-echo "172.16.4.1/28" > /etc/net/ifaces/enp7s2/ipv4address
+echo "172.16.1.1/28" > /etc/net/ifaces/enp7s2/ipv4address
 
 mkdir -p /etc/net/ifaces/enp7s3
 cat > /etc/net/ifaces/enp7s3/options <<EOF
@@ -25,13 +31,14 @@ NM_CONTROLLED=no
 SYSTEMD_CONTROLLED=yes
 DISABLED=no
 EOF
-echo "172.16.5.1/28" > /etc/net/ifaces/enp7s3/ipv4address
+echo "172.16.2.1/28" > /etc/net/ifaces/enp7s3/ipv4address
 
-echo "nameserver 8.8.8.8" > /etc/net/ifaces/enp7s1/resolv.conf
+echo "nameserver 77.88.8.8" > /etc/net/ifaces/enp7s1/resolv.conf
 systemctl restart network
 
 # Форвардинг
 echo 1 > /proc/sys/net/ipv4/ip_forward
+sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
 
 # NAT
 apt-get update && apt-get install -y iptables
