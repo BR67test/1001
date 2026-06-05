040bbb014643012e784f83bbbc54df99e8864f37
BR67 <152090761+BR67test@users.noreply.github.com>
2026-06-05 15:21:23 +0500

    Update hq_srv_setup.sh

--- hq_srv_setup.sh
+++ hq_srv_setup.sh
@@ -1,44 +1,120 @@
 #!/bin/bash
-echo "=== Настройка HQ-SRV ==="
+echo "=== HQ-SRV ==="
 
 hostnamectl set-hostname hq-srv.au-team.irpo
 
-# 1. Настройка сети
+# Сеть
 mkdir -p /etc/net/ifaces/enp7s1
-echo "BOOTPROTO=static" > /etc/net/ifaces/enp7s1/options
-echo "192.168.100.2/26" > /etc/net/ifaces/enp7s1/ipv4address
-echo "default via 192.168.100.1" > /etc/net/ifaces/enp7s1/ipv4route
-echo "nameserver 77.88.8.8" > /etc/net/ifaces/enp7s1/resolv.conf
+cat > /etc/net/ifaces/enp7s1/options <<EOF
+BOOTPROTO=manual
+TYPE=eth
+NM_CONTROLLED=no
+SYSTEMD_CONTROLLED=yes
+DISABLED=no
+EOF
+
+mkdir -p /etc/net/ifaces/enp7s1.100
+cat > /etc/net/ifaces/enp7s1.100/options <<EOF
+TYPE=vlan
+HOST=enp7s1
+VID=100
+BOOTPROTO=static
+NM_CONTROLLED=no
+SYSTEMD_CONTROLLED=yes
+DISABLED=no
+EOF
+echo "192.168.100.2/26" > /etc/net/ifaces/enp7s1.100/ipv4address
+echo "default via 192.168.100.1" > /etc/net/ifaces/enp7s1.100/ipv4route
+echo "nameserver 127.0.0.1" > /etc/net/ifaces/enp7s1.100/resolv.conf
+
 systemctl restart network
 
-# 2. DNS-сервер (Bind9)
-apt-get update && apt-get install -y bind bind-utils
-# Здесь будет конфигурация Bind из твоего документа для зон au-team.irpo и обратной
+# BIND9
+apt-get update
+apt-get install -y bind bind-utils
+
 cat > /etc/bind/options.conf <<BIND
 options {
     listen-on { 192.168.100.2; };
+    listen-on-v6 { none; };
     forwarders { 77.88.8.8; };
     allow-query { any; };
     allow-recursion { any; };
 };
 BIND
-# ... (добавление зон и их конфигурация, как в твоём документе)
+
+cat >> /etc/bind/rfc1912.conf <<ZONES
+
+zone "au-team.irpo" {
+    type master;
+    file "au-team.irpo";
+};
+
+zone "100.168.192.in-addr.arpa" {
+    type master;
+    file "100.168.192.in-addr.arpa";
+};
+ZONES
+
+cat > /etc/bind/zone/au-team.irpo <<ZONE
+\$TTL 1D
+@       IN SOA  hq-srv.au-team.irpo. root.au-team.irpo. (
+                2025010101 ; serial
+                12H        ; refresh
+                1H         ; retry
+                1W         ; expire
+                1H         ; minimum
+)
+        IN NS   hq-srv.au-team.irpo.
+        IN A    192.168.100.2
+hq-srv  IN A    192.168.100.2
+hq-rtr  IN A    192.168.100.1
+hq-cli  IN A    192.168.100.66
+br-rtr  IN A    192.168.200.1
+br-srv  IN A    192.168.200.2
+moodle  IN CNAME hq-srv
+wiki    IN CNAME hq-srv
+ZONE
+
+cat > /etc/bind/zone/100.168.192.in-addr.arpa <<ZONE
+\$TTL 1D
+@       IN SOA  hq-srv.au-team.irpo. root.au-team.irpo. (
+                2025010101 ; serial
+                12H        ; refresh
+                1H         ; retry
+                1W         ; expire
+                1H         ; minimum
+)
+        IN NS   hq-srv.au-team.irpo.
+2       IN PTR  hq-srv.au-team.irpo.
+1       IN PTR  hq-rtr.au-team.irpo.
+ZONE
+
+chown root:named /etc/bind/zone/au-team.irpo
+chown root:named /etc/bind/zone/100.168.192.in-addr.arpa
+
 systemctl enable --now bind
 
-# 3. Создание пользователя sshuser
+# Пользователь
 useradd sshuser -u 1010
 echo "sshuser:P@ssw0rd" | chpasswd
 usermod -aG wheel sshuser
 echo "sshuser ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-# 4. Настройка SSH
+# SSH
 apt-get install -y openssh-server
-sed -i 's/^#Port 22/Port 2024/' /etc/openssh/sshd_config
-sed -i 's/^#MaxAuthTries 6/MaxAuthTries 2/' /etc/openssh/sshd_config
-sed -i 's/^#PermitRootLogin yes/PermitRootLogin no/' /etc/openssh/sshd_config
-echo "AllowUsers sshuser" >> /etc/openssh/sshd_config
+cat > /etc/openssh/sshd_config <<SSH
+Port 2024
+MaxAuthTries 2
+PermitRootLogin no
+AllowUsers sshuser
+Banner /etc/openssh/banner
+Subsystem sftp /usr/libexec/openssh/sftp-server
+SSH
 echo "Authorized access only" > /etc/openssh/banner
-sed -i '/^#Banner none/a Banner /etc/openssh/banner' /etc/openssh/sshd_config
 systemctl restart sshd
 
-echo "=== HQ-SRV настроен ==="
+# Время
+timedatectl set-timezone Asia/Yekaterinburg
+
+echo "=== HQ-SRV готов ==="
