30ade2d8055a85724464c4dcd51e3416d7ef77ec
BR67 <152090761+BR67test@users.noreply.github.com>
2026-06-07 01:07:22 +0500

    Update and rename br_rtr_setup.sh to br_rtr_setup_m2.sh

---
+++ moduls_2/br_rtr_setup_m2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "=== BR-RTR (Модуль 2) ==="
+
+# Проброс портов на BR-SRV
+iptables -t nat -A PREROUTING -i enp7s1 -p tcp --dport 2026 -j DNAT --to-destination 192.168.0.2:2026
+iptables -t nat -A PREROUTING -i enp7s1 -p tcp --dport 8080 -j DNAT --to-destination 192.168.0.2:8080
+iptables-save > /etc/sysconfig/iptables
+
+# NTP-клиент (сервер — ISP)
+sed -i 's/^pool/#pool/' /etc/chrony.conf
+echo "server 172.16.2.1 iburst" >> /etc/chrony.conf
+systemctl restart chronyd
+
+echo "=== BR-RTR обновлен ==="
