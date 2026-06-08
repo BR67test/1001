3b4fa4f3a34c7ffc7e1e56c363a43f352b6400e6
BR67 <152090761+BR67test@users.noreply.github.com>
2026-06-08 23:43:28 +0500

    Create BR-RTR.sh

---
+++ moduls_2/ssh/BR-RTR.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "=== SSH настройка BR-RTR ==="
+
+apt-get update && apt-get install -y openssh-server
+
+echo "net_admin:P@ssw0rd" | chpasswd
+
+cat > /etc/openssh/sshd_config <<EOF
+Port 2026
+MaxAuthTries 3
+PermitRootLogin no
+AllowUsers net_admin
+Subsystem sftp /usr/libexec/openssh/sftp-server
+EOF
+
+systemctl enable --now sshd
+systemctl status sshd --no-pager
+
+echo "=== SSH на BR-RTR готов (порт 2026, пользователь net_admin) ==="
