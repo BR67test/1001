5dc73fafc51a504c145d1047f8ed9f80568c936f
BR67 <152090761+BR67test@users.noreply.github.com>
2026-06-09 01:01:06 +0500

    Update br_rtr_setup_m2.sh

--- moduls_2/br_rtr_setup_m2.sh
+++ moduls_2/br_rtr_setup_m2.sh
@@ -2,8 +2,8 @@
 echo "=== BR-RTR (Модуль 2) ==="
 
 # Проброс портов на BR-SRV
-iptables -t nat -A PREROUTING -i enp7s2 -p tcp --dport 2026 -j DNAT --to-destination 192.168.0.2:2026
-iptables -t nat -A PREROUTING -i enp7s2 -p tcp --dport 8080 -j DNAT --to-destination 192.168.0.2:8080
+iptables -t nat -A PREROUTING -i enp7s1 -p tcp --dport 2026 -j DNAT --to-destination 192.168.0.2:2026
+iptables -t nat -A PREROUTING -i enp7s1 -p tcp --dport 8080 -j DNAT --to-destination 192.168.0.2:8080
 iptables-save > /etc/sysconfig/iptables
 
 # NTP-клиент (сервер — ISP)
