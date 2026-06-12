16d9d732f73cc52a622b5f93a6d109d6b7e7b891
BR67 <152090761+BR67test@users.noreply.github.com>
2026-06-12 23:15:22 +0500

    Update hq_rtr_setup_m2.sh

--- moduls_2/hq_rtr_setup_m2.sh
+++ moduls_2/hq_rtr_setup_m2.sh
@@ -19,6 +19,16 @@ EOF
 systemctl enable --now sshd
 
 # ============================================
+# Настройка dnsmasq (DHCP)
+# ============================================
+if [ -f /etc/dnsmasq.conf ]; then
+    # Устанавливаем DNS сервер BR-SRV
+    sed -i 's/dhcp-option=6,.*/dhcp-option=6,192.168.3.10/' /etc/dnsmasq.conf
+    systemctl restart dnsmasq
+    echo "dnsmasq обновлён (DNS = 192.168.3.10)"
+fi
+
+# ============================================
 # Удаление старого правила и добавление нового
 # ============================================
 iptables -t nat -D PREROUTING -i enp7s1 -p tcp --dport 8080 -j DNAT --to-destination 192.168.100.2:80 2>/dev/null
