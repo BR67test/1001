a2a38a0a458ed5f0d7b2307039932f8281231616
BR67 <152090761+BR67test@users.noreply.github.com>
2026-06-14 01:22:15 +0500

    Update br_srv_setup_m2.sh

--- moduls_2/br_srv_setup_m2.sh
+++ moduls_2/br_srv_setup_m2.sh
@@ -31,14 +31,7 @@ rm -rf /var/cache/samba/
 mkdir -p /var/lib/samba/sysvol
 
 # Провизор домена
-samba-tool domain provision \
-    --realm=AU-TEAM.IRPO \
-    --domain=AU-TEAM \
-    --server-role=dc \
-    --dns-backend=SAMBA_INTERNAL \
-#    --dns-forwarder=77.88.8.8 \
-    --adminpass='P@ssw0rd' \
-    --use-rfc2307
+samba-tool domain provision --realm=AU-TEAM.IRPO --domain=AU-TEAM --server-role=dc --dns-backend=SAMBA_INTERNAL --adminpass='P@ssw0rd' --use-rfc2307
 
 cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
 
